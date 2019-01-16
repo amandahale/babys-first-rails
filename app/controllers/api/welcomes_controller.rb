cfd94e51d9bd8841c0c77d6b56aac250ae3ea870
amanda <forshagonoff@gmail.com>
2019-01-16 10:16:44 -0600

    updates babys repo

---
+++ app/controllers/api/welcomes_controller.rb
@@ -0,0 +1,12 @@
+
+class Api::WelcomesController < ApplicationController
+  def hello
+    @random = Random.new.rand(100)
+    render 'hello.json.jbuilder'
+  end
+
+  def about
+    @language = "ruby"
+    render 'about.json.jbuilder'
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  
+  get "/welcomes" => "api/welcomes#hello"
+
+  get "/about" => "api/welcomes#about"
+end
